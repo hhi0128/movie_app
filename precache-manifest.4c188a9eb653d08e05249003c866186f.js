67e732d640c1e30c17b6a5f1c3b0856abc2662c9
hhi0128h <hhi0128h@gmail.com>
2019-03-18 18:28:11 +0900

    Updates

---
+++ precache-manifest.4c188a9eb653d08e05249003c866186f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.e6fc6960.js"
+  },
+  {
+    "revision": "e95b7c881ce985aee7e2",
+    "url": "/movie_app/static/js/main.14f307c2.chunk.js"
+  },
+  {
+    "revision": "79876ddfb540893b229e",
+    "url": "/movie_app/static/js/2.28db7df0.chunk.js"
+  },
+  {
+    "revision": "e95b7c881ce985aee7e2",
+    "url": "/movie_app/static/css/main.a1da79ee.chunk.css"
+  },
+  {
+    "revision": "0522d8c420fb0c8dae967250c5e77ca5",
+    "url": "/movie_app/index.html"
+  }
+];
